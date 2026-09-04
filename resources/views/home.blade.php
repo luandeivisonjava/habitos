2742e1cf122a6409b0d219a10edc6f3d65a78b0d
luandeivisonjava <luandeivison.java@gmail.com>
2026-09-03 21:57:08 -0300

    setup tailiwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,17 @@
-<h1>Habit Tracker HOME</h1>
+<!DOCTYPE html>
+<html lang="pt-BR" class="h-full">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Acompanhe seus hábitos e construa uma rotina melhor, um dia de cada vez.">
+  <title>{{ config('app.name') }}</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+  <h1 class="text-3xl font-bold underline">Bem vindo ao {{ config('app.name', 'Habit Tracker') }}
+  </h1>
+</body>
+
+</html>
